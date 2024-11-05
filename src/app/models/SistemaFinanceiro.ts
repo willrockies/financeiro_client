290d6bf0b6a9228225d04c911df291e193a25176
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-11-05 17:39:22 -0300

    AULA 10 - ANGULAR - FINANCEIRO (FRONT-END) implementação chamada da Api cadastro sistema financeiro

--- src/app/models/SistemaFinanceiro.ts
+++ src/app/models/SistemaFinanceiro.ts
@@ -1,12 +1,14 @@
 
 export class SistemaFinanceiro  {
-  Id: number;
-  Nome: number;
-  Mes: number;
-  Ano: number;
-  DiaFechamento: number;
-  GerarCopiaDespesa: boolean;
-  MesCopia: number;
-  AnoCopia: number;
-
+  id!: number;
+  Nome!: number;
+  Mes!: number;
+  Ano!: number;
+  DiaFechamento!: number;
+  GerarCopiaDespesa!: boolean;
+  MesCopia!: number;
+  AnoCopia!: number;
+  NomePropriedade:string="";
+  mensagem:string="";
+  notificacoes:any = [];
 }
--- src/app/pages/sistema/sistema.component.ts
+++ src/app/pages/sistema/sistema.component.ts
@@ -1,6 +1,8 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { MenuService } from 'src/app/services/menu.service';
+import { SistemaFinanceiro } from 'src/app/models/SistemaFinanceiro';
+import { SistemaService } from 'src/app/services/sistema.service';
 
 @Component({
   selector: 'app-sistema',
@@ -9,7 +11,10 @@ import { MenuService } from 'src/app/services/menu.service';
 })
 export class SistemaComponent {
   sistemaForm: FormGroup = new FormGroup({});
-  constructor(public menuService: MenuService, public formBuilder:FormBuilder) { }
+  constructor(
+    public menuService: MenuService,
+    public formBuilder: FormBuilder,
+    public sistemaService: SistemaService) { }
 
 
   ngOnInit() {
@@ -22,16 +27,35 @@ export class SistemaComponent {
   }
 
   dadosForm() {
-   return this.sistemaForm?.controls;
+    return this.sistemaForm?.controls;
   }
 
   enviar() {
-    //debugger;
-    if(this.sistemaForm?.valid){
-      var dados = this.dadosForm();
-    }
-    console.log("campo obrigatorio");
-    throw new Error("campo obrigatorio");
+
+
+    var dados = this.dadosForm();
+
+    let item = new SistemaFinanceiro();
+    item.Nome = dados["name"].value;
+    item.id =0;
+    item.Mes = 0;
+    item.Ano = 0;
+    item.DiaFechamento = 0;
+    item.GerarCopiaDespesa = true;
+    item.MesCopia = 0;
+    item.AnoCopia = 0;
+
+    item.NomePropriedade;
+    item.mensagem;
+    item.notificacoes;
+    this.sistemaService.adicionarSistemaFinanceiro(item).subscribe((res: any) => {
+      this.sistemaForm.reset();
+      this.sistemaService.cadastrarUsuarioNoSistema(res.result.id, "wilson2@gmail.com")
+        .subscribe((response: any) => {
+          debugger;
+        }), (error:any) => console.error(error), () => { }
+
+    }), (error:any) => console.error(error), () => { }
 
   }
 }
