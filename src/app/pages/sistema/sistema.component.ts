87408ee661cd55fba4e08bc718ac272706be0ba2
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-11-14 15:24:00 -0300

    AULA 12 - ANGULAR - FINANCEIRO (FRONT-END) Criando modelo da tela de listagem

--- src/app/pages/sistema/sistema.component.ts
+++ src/app/pages/sistema/sistema.component.ts
@@ -11,7 +11,17 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./sistema.component.scss']
 })
 export class SistemaComponent {
+  tipoTela: number = 1;// 1 - listagem, 2 - cadastro, 3 edição;
+  tableListSistemas!: Array<SistemaFinanceiro>;
+  id!: string;
   sistemaForm: FormGroup = new FormGroup({});
+
+  page: number = 1;
+  config: any;
+  paginacao: boolean = true;
+  itemsPorPagina: number = 10;
+
+
   constructor(
     public menuService: MenuService,
     public formBuilder: FormBuilder,
@@ -21,11 +31,58 @@ export class SistemaComponent {
 
   ngOnInit() {
     this.menuService.menuSelecionado = 2;
-
+    this.configpag();
     this.sistemaForm = this.formBuilder.group({
       name: ['', [Validators.required]]
-
     });
+
+    this.ListaSistemasUsuario();
+  }
+
+  configpag() {
+    this.id = this.gerarIdParaConfigDePaginacao();
+
+    this.config = {
+      id: this.id,
+      currentPage: this.page,
+      itemsPerPage: this.itemsPorPagina
+
+    };
+  }
+
+  gerarIdParaConfigDePaginacao() {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 10; i++) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
+  }
+
+  ListaSistemasUsuario() {
+    this.tipoTela = 1;
+    //debugger;
+    let getUserLogado = this.authService.getEmailUser();
+    this.sistemaService.listaSistemasUsuario(getUserLogado)
+      .subscribe((response: Array<SistemaFinanceiro>) => {
+        this.tableListSistemas = response;
+      }, (error) => console.error(error), () => { })
+  }
+  cadastro() {
+    this.tipoTela = 2;
+    this.sistemaForm.reset();
+  }
+
+  mudarPage($event: number) {
+    this.page = $event;
+    this.config.currentPage = this.page;
+  }
+  mudarItemsPorPage() {
+    this.page = 1
+    this.config.currentPage = this.page;
+    this.config.itemsPerPage = this.itemsPorPagina;
   }
 
   dadosForm() {
@@ -37,7 +94,7 @@ export class SistemaComponent {
 
     let item = new SistemaFinanceiro();
     item.nome = dados["name"].value;
-    item.id =0;
+    item.id = 0;
     item.mes = 0;
     item.ano = 0;
     item.diaFechamento = 0;
@@ -56,9 +113,9 @@ export class SistemaComponent {
       this.sistemaService.cadastrarUsuarioNoSistema(res.result.id, getUserLogado)
         .subscribe((response: any) => {
           debugger;
-        }), (error:any) => console.error(error), () => { }
+        }), (error: any) => console.error(error), () => { }
 
-    }), (error:any) => console.error(error), () => { }
+    }), (error: any) => console.error(error), () => { }
 
   }
 }
--- src/app/pages/sistema/sistema.module.ts
+++ src/app/pages/sistema/sistema.module.ts
@@ -4,8 +4,10 @@ import { SistemaComponent } from './sistema.component';
 import { SistemaRoutingModule } from './sistema-routing.module';
 import { NavBarModule } from '../../components/navbar/navbar.module';
 import { SideBarModule } from '../../components/sidebar/sidebar.module';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     SistemaComponent
@@ -15,7 +17,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     SistemaRoutingModule,
     NavBarModule,
     SideBarModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    FormsModule,
+    NgSelectModule,
+    MatIconModule
+
   ]
 })
 export class SistemaModule { }
